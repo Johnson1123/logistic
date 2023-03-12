547b6899ddd77a32a2347d51d24c71b3fa52044f
unknown <onifadejohnson2017@gmail.com>
2023-03-12 10:47:25 -0500

    netlify correction

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,7 @@ import './style/utils.scss'
 import { Provider } from 'react-redux';
 import { store } from './App/store';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
