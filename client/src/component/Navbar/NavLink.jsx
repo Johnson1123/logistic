b175eadcc1cd8082b8f6af7ed73a3b9bcb6a2f2a
unknown <onifadejohnson2017@gmail.com>
2023-03-12 15:51:44 -0500

    navbar correction

--- client/src/component/Navbar/NavLink.jsx
+++ client/src/component/Navbar/NavLink.jsx
@@ -3,7 +3,8 @@ import { TfiAngleRight } from 'react-icons/tfi'
 
 export default function NavLink(props) {
   return (
-    <div className="menu__item flex-center">
+    
+    <div className="menu__item flex">
         <li className='link normal-text'>{props.label}</li>
         <TfiAngleRight />
     </div>
